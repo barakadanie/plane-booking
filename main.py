0d6c2f72c9a6fba76f00f560677e179ad2f72ec9
barakadanie <74895402+barakadanie@users.noreply.github.com>
2021-08-03 16:19:53 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+class home:
+    def __init__(self,book):
+        self.book = book
+
+    def __init__(self,cancel):
+        self.cancel = cancel
+
+    def __init__(self,checklist):
+        self.checklist = checklist
